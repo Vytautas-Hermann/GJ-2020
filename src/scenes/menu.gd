99e5a50cc7deaf9979d26af44c23007054b3e04e
Elena Abraham <elena.abraham@mni.thm.de>
2020-04-17 19:15:45 +0200

    menu

---
+++ src/scenes/menu.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$MenuButton.get_popup().add_item("Sound on/off")
+	$MenuButton.get_popup().add_item("Main Menu")
+
+
+
