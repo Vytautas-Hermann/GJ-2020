96c975144d8365b2c030da2b7d57cc3755865537
Vytautas Hermann <vytautas.hermann@mni.thm.de>
2020-04-17 22:48:58 +0200

    more code

--- src/scripts/menu.gd
+++ src/scripts/menu.gd
@@ -1,17 +1,18 @@
 extends CanvasLayer
 
+var score = 0
+var money = 100
+var health = 100
+var maze = true;
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$MoneyText.text="Money: %s" % money
+	$HealthText.text="Health: %s" % health
+	$ScoreText.text="Score: %s" % score
 	$MenuButton.get_popup().add_item("Sound on/off")
 	$MenuButton.get_popup().add_item("Main Menu")
-	
 	$MenuButton.get_popup().connect("id_pressed", self, "_on_item_pressed")
+	$Button.connect("pressed", self,"_swap")
 
 func _on_item_pressed(id):
 	var item_name = $MenuButton.get_popup().get_item_text(id)
@@ -20,3 +21,40 @@ func _on_item_pressed(id):
 
 func _on_mainMenu():
 	get_tree().change_scene("res://src/scenes/Title_Screen.tscn")
+
+func _swap():
+	if maze:
+		get_parent().offset = Vector2(0,1100)
+		maze=false
+	else:
+		get_parent().offset = Vector2(0,0)
+		maze=true
+
+func _change_money(var bal):
+	money += bal
+	$MoneyText.text="Money: %s" % money
+
+func _decrease_health(var loss):
+	health -= loss
+	$HealthText.text="Health: %s" % health
+
+func _increase_score(var add):
+	score += add
+	$ScoreText.text="Score: %s" % score
+
+func _set_maze_built():
+	for child in $BuildMenue.get_popup().get_children():
+		child.queue_free()
+	$BuildMenue.get_popup().add_item("Plate")
+	$BuildMenue.get_popup().add_item("Big Plate")
+	$BuildMenue.get_popup().connect("id_pressed", self, "_on_build_pressed")
+
+func _set_kit_built():
+	for child in $BuildMenue.get_popup().get_children():
+		child.queue_free()
+	$BuildMenue.get_popup().add_item("Rice Cooker")
+	$BuildMenue.get_popup().add_item("Fries")
+	$BuildMenue.get_popup().connect("id_pressed", self, "_on_build_pressed")
+
+func _on_build_pressed():
+	pass
