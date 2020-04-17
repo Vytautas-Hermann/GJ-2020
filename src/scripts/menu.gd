1eff57f1a65077b7dbf22c2b4c4d32d6324daeab
Elena Abraham <elena.abraham@mni.thm.de>
2020-04-17 19:34:22 +0200

    main menu button

--- src/scripts/menu.gd
+++ src/scripts/menu.gd
@@ -10,6 +10,13 @@ extends CanvasLayer
 func _ready() -> void:
 	$MenuButton.get_popup().add_item("Sound on/off")
 	$MenuButton.get_popup().add_item("Main Menu")
+	
+	$MenuButton.get_popup().connect("id_pressed", self, "_on_item_pressed")
 
+func _on_item_pressed(id):
+	var item_name = $MenuButton.get_popup().get_item_text(id)
+	if item_name == "Main Menu":
+		_on_mainMenu()
 
-
+func _on_mainMenu():
+	get_tree().change_scene("res://src/scenes/Title_Screen.tscn")
