e3d43c10554e4c381408b44dcec42fb0c3f178aa
Tim Lucas Weber <tim.weber-2@mni.thm.de>
2020-04-19 03:44:53 +0200

    endscreen working with tiny font

--- src/scenes/Score.gd
+++ src/scenes/Score.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Node/MarginContainer/VBoxContainer/Score.text = str(global.score)
+	$Node/Sprite/Score.text = str(global.score)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
