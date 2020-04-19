cc6e6b7192e6c973965a100156d588b7e2dd2823
Vytautas Hermann <vytautas.hermann@mni.thm.de>
2020-04-19 02:32:30 +0200

    ghghj

--- src/scripts/Import.gd
+++ src/scripts/Import.gd
@@ -24,6 +24,40 @@ func _init_prod():
 func _on_build_pressed(id):
 	if id < 17:
 		product = id
+		if id == 0:
+			$Sprite.texture = load("res://.import/rat.png-18e23395d4c5625002064d339ca77bcc.stex")
+		if id == 1:
+			$Sprite.texture = load("res://.import/chicken.png-074e4d8f881c4892ddea06671e224a42.stex")
+		if id == 2:
+			$Sprite.texture = load("res://.import/pig.png-b4453315f94ec4fb927db85e58c20b4f.stex")
+		if id == 3:
+			$Sprite.texture = load("res://.import/cow.png-e9e525806d0c2fe42cf1e03de04a15bf.stex")
+		if id == 4:
+			$Sprite.texture = load("res://.import/deer.png-bf3378c6d38d426bfa65f7b793eaa669.stex")
+		if id == 5:
+			$Sprite.texture = load("res://.import/cheese.png-2832f92752a3c924e2cba04504a7e1a6.stex")
+		if id == 6:
+			$Sprite.texture = load("res://.import/potatoes.png-423bcf4cf2661c494e2cc3cd45ca5ea2.stex")
+		if id == 7:
+			$Sprite.texture = load("res://.import/cucumber.png-8fad98e32703c82ad6bd2c8c7a9e7bc1.stex")
+		if id == 8:
+			$Sprite.texture = load("res://.import/tomato.png-f567a29a221bb5dcd24118b4831489b6.stex")
+		if id == 9:
+			$Sprite.texture = load("res://.import/sala.png-493f223b164c141c2c0662426252157e.stex")
+		if id == 10:
+			$Sprite.texture = load("res://.import/rice.png-8d434eae9fa95399dd13a71fa1aab2f3.stex")
+		if id == 11:
+			$Sprite.texture = load("res://.import/bread1.png-565f4f56f3f11c6368cfc35214f05b63.stex")
+		if id == 12:
+			$Sprite.texture = load("res://.import/pasta.png-dae4e954950642bb36adf1108095eba9.stex")
+		if id == 13:
+			$Sprite.texture = load("res://.import/spaguetti.png-65e5cd39bc0ef66de0739d56335d4865.stex")
+		if id == 14:
+			$Sprite.texture = load("res://.import/cook.png-0cd898fa6a1d2c8ccbd6dd1d2857e089.stex")
+		if id == 15:
+			$Sprite.texture = load("res://.import/jelly.png-8b65f49144f6e9e36b759fa167a28d48.stex")
+		if id == 16:
+			$Sprite.texture = load("res://.import/honey.png-d7f5dae6f144aed71f1b4ecaf8a4befb.stex")
 	elif id == 17:
 		if get_node("/root/Game/Camera2D/CanvasLayer")._change_money(-5*pow(1.25,level)):
 			level += 1
